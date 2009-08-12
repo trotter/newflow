b6ebf526636aa19ae141c15726c925db4609a6ac
Trotter Cashion <cashion@gmail.com>
2009-08-11 20:34:31 -0400

    Add transition specs

---
+++ spec/newflow_transition_spec.rb
@@ -0,0 +1,88 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "An :if symbol transition with a symbol trigger" do
+  before do
+    @workflow = mock("workflow")
+    @transition = Newflow::Transition.new(:target_state, :if => :predicate?, :trigger => :some_action)
+  end
+
+  it "should not be able to transition if predicate is false" do
+    @workflow.should_receive(:predicate?).and_return false
+    @transition.can_transition?(@workflow).should be_false
+  end
+
+  it "should be able to transition if predicate is true" do
+    @workflow.should_receive(:predicate?).and_return true
+    @transition.can_transition?(@workflow).should be_true
+  end
+
+  it "should call the trigger if requested" do
+    @workflow.should_receive(:some_action)
+    @transition.trigger!(@workflow)
+  end
+end
+
+describe "An :unless symbol transition with a proc trigger" do
+  before do
+    @workflow = mock("workflow")
+    @trigger_ran = false
+    @trigger  = lambda { @trigger_ran = true }
+    @transition = Newflow::Transition.new(:target_state, :unless => :predicate?, :trigger => @trigger)
+  end
+
+  it "should not be able to transition if predicate is true" do
+    @workflow.should_receive(:predicate?).and_return true
+    @transition.can_transition?(@workflow).should be_false
+  end
+
+  it "should be able to transition if predicate is false" do
+    @workflow.should_receive(:predicate?).and_return false
+    @transition.can_transition?(@workflow).should be_true
+  end
+
+  it "should call the trigger when requested" do
+    @transition.trigger!(@workflow)
+    @trigger_ran.should be_true
+  end
+end
+
+describe "An :if proc transition with no trigger" do
+  before do
+    @workflow = mock("workflow")
+    @if_proc  = lambda { @predicate_value }
+    @transition = Newflow::Transition.new(:target_state, :if => @if_proc)
+  end
+
+  it "should not be able to transition if predicate is false" do
+    @predicate_value = false
+    @transition.can_transition?(@workflow).should be_false
+  end
+
+  it "should be able to transition if predicate is true" do
+    @predicate_value = true
+    @transition.can_transition?(@workflow).should be_true
+  end
+
+  it "should do nothing when triggered" do
+    lambda { @transition.trigger!(@workflow) }.should_not raise_error
+  end
+end
+
+describe "An :unless proc transition with no trigger" do
+  before do
+    @workflow    = mock("workflow")
+    @unless_proc = lambda { @predicate_value }
+    @transition  = Newflow::Transition.new(:target_state, :unless => @unless_proc)
+  end
+
+  it "should not be able to transition if predicate is true" do
+    @predicate_value = true
+    @transition.can_transition?(@workflow).should be_false
+  end
+
+  it "should be able to transition if predicate is false" do
+    @predicate_value = false
+    @transition.can_transition?(@workflow).should be_true
+  end
+end
+
